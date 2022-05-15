2a882d7c65dd6d06bf003b2ca28ba12b96684b80
Oleksii Horban <lehagorban93@gmail.com>
2022-05-15 21:15:53 +0300

    task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,26 @@
-const categoriesEl = document.querySelector("#categories") 
+function createElement() {
+    const categories = document.querySelector("#categories")
     
-console.log('Number of categories:', categoriesEl.children.length);
-console.log("Category:", categoriesEl.firstElementChild.querySelector('h2').textContent);
-console.log('Elements:', categoriesEl.querySelector('ul').children.length);
-console.log('Category:', categoriesEl.children[1].querySelector('h2').textContent);
-console.log('Elements:', categoriesEl.children[1].querySelector('ul').children.length);
-console.log('Category:', categoriesEl.lastElementChild.querySelector('h2').textContent);
-console.log('Elements:', categoriesEl.lastElementChild.querySelector('ul').children.length);
+const numberOfCategories = document.querySelectorAll('#categories ul');
+console.log('Number of categories:', numberOfCategories.length);
+const categoriesItem = document.querySelectorAll('.item');
+ for (const item of categoriesItem) {
+        const categoriesTitle = item.querySelector('#categories h2');
+        const categoriesLength = item.querySelectorAll('ul li');
+        console.log(`Category: ${categoriesTitle.textContent}`);
+        console.log(`Elements: ${categoriesLength.length}`);
+    }
+}
+
+
+createElement()
+
+    
+// console.log('Number of categories:', categoriesEl.children.length);
+// console.log("Category:", categoriesEl.firstElementChild.querySelector('h2').textContent);
+// console.log('Elements:', categoriesEl.querySelector('ul').children.length);
+// console.log('Category:', categoriesEl.children[1].querySelector('h2').textContent);
+// console.log('Elements:', categoriesEl.children[1].querySelector('ul').children.length);
+// console.log('Category:', categoriesEl.lastElementChild.querySelector('h2').textContent);
+// console.log('Elements:', categoriesEl.lastElementChild.querySelector('ul').children.length);
+
